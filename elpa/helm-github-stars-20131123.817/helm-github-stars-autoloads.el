0d61768e5c5dd79f05ec54b023372efbe0a2f919
Cristian <knur@tabatas-air.gateway.pace.com>
2014-05-03 21:00:08 -0700

    working branch

---
+++ elpa/helm-github-stars-20131123.817/helm-github-stars-autoloads.el
@@ -0,0 +1,29 @@
+;;; helm-github-stars-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (helm-github-stars) "helm-github-stars" "helm-github-stars.el"
+;;;;;;  (21221 36426 0 0))
+;;; Generated autoloads from helm-github-stars.el
+
+(autoload 'helm-github-stars "helm-github-stars" "\
+Show and Browse your github's stars.
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("helm-github-stars-pkg.el") (21221 36426
+;;;;;;  213015 0))
+
+;;;***
+
+(provide 'helm-github-stars-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; helm-github-stars-autoloads.el ends here
