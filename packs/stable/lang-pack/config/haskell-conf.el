8def1ae2a69847afc12ee58a6064c09211b90d8d
Cristian <me@cristian.io>
2014-11-23 19:15:11 -0800

    Updated lang pack

--- packs/stable/lang-pack/config/haskell-conf.el
+++ packs/stable/lang-pack/config/haskell-conf.el
@@ -3,7 +3,6 @@
 (require 'haskell-simple-indent)
 (require 'haskell-mode)
 
-
 (add-to-list 'auto-mode-alist        '("\\.\\(?:[gh]s\\|hi\\)\\'" . haskell-mode))
 (add-to-list 'auto-mode-alist        '("\\.l[gh]s\\'" . literate-haskell-mode))
 (add-to-list 'interpreter-mode-alist '("runghc" . haskell-mode))
